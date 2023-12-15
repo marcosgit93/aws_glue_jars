e8397695bcfeebf5c28c62e62b5cf268d34592e3
marcosgit93 <vickkmarcos@gmail.com>
2023-12-15 12:41:15 -0300

    Update upload_s3_multitreding.py

--- upload_s3_multitreding.py
+++ upload_s3_multitreding.py
@@ -2,39 +2,38 @@ import os
 import concurrent.futures
 import boto3
 
-# Função para fazer o upload de um arquivo para o S3
-def upload_file(bucket_name, file_path, s3_key):
-    s3 = boto3.client('s3')
-    s3.upload_file(file_path, bucket_name, s3_key)
+# Configurações do AWS S3
+BUCKET_NAME = 'seu-bucket'
+AWS_ACCESS_KEY_ID = 'seu-access-key'
+AWS_SECRET_ACCESS_KEY = 'seu-secret-key'
+REGION_NAME = 'sua-regiao-do-bucket'
 
-# Função para fazer upload de uma pasta para o S3 de forma multitarefa
-def upload_folder_to_s3(bucket_name, folder_path, s3_prefix):
-    s3 = boto3.client('s3')
-    files_to_upload = []
+# Função para realizar o upload de uma pasta para o AWS S3
+def upload_folder(folder_path, s3_client, bucket_name):
     for root, dirs, files in os.walk(folder_path):
         for file in files:
-            file_path = os.path.join(root, file)
-            s3_key = s3_prefix + file_path[len(folder_path) + 1:]
-            files_to_upload.append((file_path, s3_key))
+            local_path = os.path.join(root, file)
+            s3_path = os.path.relpath(local_path, folder_path)
+            s3_client.upload_file(local_path, bucket_name, s3_path)
+            print(f'Uploaded {local_path} to S3 bucket')
+
+if __name__ == "__main__":
+    s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                      region_name=REGION_NAME)
+
+    folder_path = 'C:/windows/teste'  # Caminho da pasta local a ser carregada para o S3
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         futures = []
-        for file_path, s3_key in files_to_upload:
-            futures.append(executor.submit(upload_file, bucket_name, file_path, s3_key))
-        
-        # Espera até que todas as threads tenham concluído
+        for root, dirs, files in os.walk(folder_path):
+            for dir_name in dirs:
+                full_path = os.path.join(root, dir_name)
+                futures.append(executor.submit(upload_folder, full_path, s3, BUCKET_NAME))
+
+        # Espera todas as threads terminarem
         for future in concurrent.futures.as_completed(futures):
             try:
                 future.result()
             except Exception as e:
-                print(f"Erro ao fazer upload: {e}")
-
-# Defina suas credenciais AWS antes de usar
-# boto3.setup_default_session(profile_name='your_profile_name')
-
-# Chame a função para fazer upload da pasta para o S3
-bucket_name = 'nome_do_seu_bucket'
-folder_path = '/caminho/da/sua/pasta'
-s3_prefix = 'prefixo_para_o_seu_s3/'  # O prefixo que você deseja no S3
-
-upload_folder_to_s3(bucket_name, folder_path, s3_prefix)
+                print(f'Exception: {e}')
