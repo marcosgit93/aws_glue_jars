b6ef798cad6971fa7eca8d19b9dccf492665a7ea
marcosgit93 <vickkmarcos@gmail.com>
2024-01-02 19:08:00 -0300

    Update download_async.py

--- download_async.py
+++ download_async.py
@@ -2,33 +2,41 @@ import os
 import boto3
 import concurrent.futures
 
-def download_file(bucket_name, key, local_directory):
-    s3 = boto3.client('s3')
-    file_name = os.path.join(local_directory, os.path.basename(key))
-    s3.download_file(bucket_name, key, file_name)
-    print(f'Arquivo baixado: {key}')
+def download_file(bucket_name, key, local_directory, s3_resource):
+    # Verificar se o arquivo está no Glacier Deep Archive
+    obj = s3_resource.Object(bucket_name, key)
+    storage_class = obj.storage_class
+    if storage_class == 'DEEP_ARCHIVE':
+        print(f"O arquivo {key} está armazenado no Glacier Deep Archive. Não será baixado.")
+        return
+
+    local_filename = os.path.join(local_directory, key)
+    os.makedirs(os.path.dirname(local_filename), exist_ok=True)
+
+    try:
+        s3_resource.meta.client.download_file(bucket_name, key, local_filename)
+        print(f"Arquivo {key} baixado com sucesso para {local_filename}")
+    except Exception as e:
+        print(f"Erro ao baixar o arquivo {key}: {str(e)}")
 
 def download_files_from_s3(bucket_name, folder_path, local_directory, num_threads=10):
-    s3 = boto3.client('s3')
-    
-    objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_path)['Contents']
-    keys = [obj['Key'] for obj in objects]
-    
-    if not os.path.exists(local_directory):
-        os.makedirs(local_directory)
-    
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        future_to_key = {executor.submit(download_file, bucket_name, key, local_directory): key for key in keys}
-        for future in concurrent.futures.as_completed(future_to_key):
-            key = future_to_key[future]
-            try:
-                future.result()
-            except Exception as e:
-                print(f'Erro ao baixar arquivo {key}: {e}')
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
 
-# Para baixar os arquivos:
-bucket_name = 'seu_bucket'
-folder_path = 'caminho/para/seu/diretorio/no/S3'
+    # Listar objetos no caminho especificado no S3
+    objects_to_download = [obj.key for obj in bucket.objects.filter(Prefix=folder_path)]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = []
+        for key in objects_to_download:
+            futures.append(executor.submit(download_file, bucket_name, key, local_directory, s3))
+
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
+
+# Exemplo de uso:
+bucket_name = 'seu-bucket'
+folder_path = 'caminho/para/os/arquivos'
 local_directory = '/caminho/local/para/salvar/os/arquivos'
 
 download_files_from_s3(bucket_name, folder_path, local_directory)
